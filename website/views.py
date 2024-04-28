6f2d105e15ed67a3edb0649b60d1cd1a77a61cb8
gotlanf <reyhanetashakori84@gmail.com>
2024-04-28 18:27:33 +0330

    add html

---
+++ website/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+
+def index_view(request):
+    return render(request, 'website/index.html')
+
+
+def about_view(request):
+    return render(request, 'website/about-us.html')
+
+
+def contact_view(request):
+    return render(request, 'website/contact.html')
+
+# Create your views here.
