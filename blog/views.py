84641eabe075bd6f177eba35ba68925dcfb5cfba
gotlanf <reyhanetashakori84@gmail.com>
2024-05-05 18:37:59 +0330

    2/16

---
+++ blog/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+
+def blog_view(request):
+    return render(request, 'blog/blog.html')
+
+
+def single_view(request):
+    return render(request, 'blog/single-blog.html')
+
+
+
+# Create your views here.
